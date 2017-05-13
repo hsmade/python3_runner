6435eb95e86b60bae2c40b6bdf1ca1136bb1659c
Wim Fournier <wim@fournier.nl>
2017-05-14 00:38:54 +0200

    updated from python2 runner

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo $@
-set -x
 if ([ -z "$1" -o -z "$2" ]) 
 then
 	echo "Syntax: docker run -ti hsmade/python3_runner https://github.com/test/me.git run_me.py arg1 arg2"
@@ -15,13 +13,21 @@ shift
 token=""
 if ([ -s /secrets/vault ] &&  $(echo $GIT|grep -q @))
 then
+	echo "Adding private token to git URL"
 	. /secrets/vault
 	prefix=$(echo $GIT|cut -d@ -f1)
 	suffix=$(echo $GIT|cut -d@ -f2-)
 	GIT="$prefix:$token@$suffix"
 fi
 
+echo "Cloning git repo"
 git clone "${GIT}${TOKEN}" /build
 cd /build
-test -f requirements.txt && pip install -r requirements.txt
+if ([ -f requirements.txt ])
+then
+	echo "Installing python requirements"
+	cat requirements.txt
+	pip install -r requirements.txt
+fi
+echo "Starting main program $PROG $@"
 python "$PROG" $@
