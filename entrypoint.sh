59938a365c7f74b3c27dbcb020f0da74667189fb
Wim Fournier <wim@fournier.nl>
2017-05-14 00:11:16 +0200

    Fixing private token use

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo $@
+set -x
 if ([ -z "$1" -o -z "$2" ]) 
 then
 	echo "Syntax: docker run -ti hsmade/python3_runner https://github.com/test/me.git run_me.py arg1 arg2"
@@ -11,12 +13,15 @@ PROG=$1
 shift
 
 token=""
-if ([ -s /secrets/vault ])
+if ([ -s /secrets/vault ] &&  $(echo $GIT|grep -q @))
 then
 	. /secrets/vault
+	prefix=$(echo $GIT|cut -d@ -f1)
+	suffix=$(echo $GIT|cut -d@ -f2-)
+	GIT="$prefix:$token@$suffix"
 fi
 
-git clone "${GIT}${token}" /build
+git clone "${GIT}${TOKEN}" /build
 cd /build
 test -f requirements.txt && pip install -r requirements.txt
 python "$PROG" $@
